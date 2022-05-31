6ca64e191dc1eaf641f9d1b7c66f7dcfec3ef153
Moisés Sansigolo <moises.sansigolo@gmail.com>
2022-05-31 15:40:27 -0300

    integrando Api ao Angular

--- SmartSchool-WebApi/Startup.cs
+++ SmartSchool-WebApi/Startup.cs
@@ -49,6 +49,8 @@ namespace SmartSchool_WebApi
 
             app.UseAuthorization();
 
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
